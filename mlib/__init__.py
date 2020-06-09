fae648994f78988f52475fef4aeca2801f8046ad
marti-1 <martynas.miliauskas@protonmail.com>
2020-06-09 13:41:34 +0300

    added candlestick_ohlc

--- mlib/__init__.py
+++ mlib/__init__.py
@@ -1,3 +1,4 @@
 from .main import *
+__all__ = ["plots"]
 
 name = "mlib"
---
+++ mlib/plots.py
@@ -0,0 +1,125 @@
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+
+
+def candlestick_ohlc(ax, quotes, width=0.2, colorup='k', colordown='r',
+                     alpha=1.0):
+    """
+    Plot the time, open, high, low, close as a vertical line ranging
+    from low to high.  Use a rectangular bar to represent the
+    open-close span.  If close >= open, use colorup to color the bar,
+    otherwise use colordown
+    Parameters
+    ----------
+    ax : `Axes`
+        an Axes instance to plot to
+    quotes : sequence of (time, open, high, low, close, ...) sequences
+        As long as the first 5 elements are these values,
+        the record can be as long as you want (e.g., it may store volume).
+        time must be in float days format - see date2num
+    width : float
+        fraction of a day for the rectangle width
+    colorup : color
+        the color of the rectangle where close >= open
+    colordown : color
+         the color of the rectangle where close <  open
+    alpha : float
+        the rectangle alpha level
+    Returns
+    -------
+    ret : tuple
+        returns (lines, patches) where lines is a list of lines
+        added and patches is a list of the rectangle patches added
+    """
+    return _candlestick(ax, quotes, width=width, colorup=colorup,
+                        colordown=colordown,
+                        alpha=alpha, ochl=False)
+
+
+def _candlestick(ax, quotes, width=0.2, colorup='k', colordown='r',
+                 alpha=1.0, ochl=True):
+    """
+    Plot the time, open, high, low, close as a vertical line ranging
+    from low to high.  Use a rectangular bar to represent the
+    open-close span.  If close >= open, use colorup to color the bar,
+    otherwise use colordown
+    Parameters
+    ----------
+    ax : `Axes`
+        an Axes instance to plot to
+    quotes : sequence of quote sequences
+        data to plot.  time must be in float date format - see date2num
+        (time, open, high, low, close, ...) vs
+        (time, open, close, high, low, ...)
+        set by `ochl`
+    width : float
+        fraction of a day for the rectangle width
+    colorup : color
+        the color of the rectangle where close >= open
+    colordown : color
+         the color of the rectangle where close <  open
+    alpha : float
+        the rectangle alpha level
+    ochl: bool
+        argument to select between ochl and ohlc ordering of quotes
+    Returns
+    -------
+    ret : tuple
+        returns (lines, patches) where lines is a list of lines
+        added and patches is a list of the rectangle patches added
+    """
+
+    OFFSET = width / 2.0
+
+    lines = []
+    patches = []
+    for q in quotes:
+        if ochl:
+            t, open, close, high, low = q[:5]
+        else:
+            t, open, high, low, close = q[:5]
+
+        if close >= open:
+            color = colorup
+            lower = open
+            height = close - open
+        else:
+            color = colordown
+            lower = close
+            height = open - close
+
+        vline = Line2D(
+            xdata=(t, t), ydata=(low, high),
+            color=color,
+            linewidth=0.5,
+            antialiased=True,
+        )
+
+        rect = Rectangle(
+            xy=(t - OFFSET, lower),
+            width=width,
+            height=height,
+            facecolor=color,
+            edgecolor=color,
+        )
+        rect.set_alpha(alpha)
+
+        lines.append(vline)
+        patches.append(rect)
+        ax.add_line(vline)
+        ax.add_patch(rect)
+    ax.autoscale_view()
+
+    return lines, patches
+
+# import datetime
+# import matplotlib.pyplot as plt
+# from matplotlib.dates import date2num
+# ohlc = [
+#     [date2num(datetime.datetime(2020, 6, 1)), 1, 2, .5, 1.5],
+#     [date2num(datetime.datetime(2020, 6, 2)), 1, 2, .5, .8]
+# ]
+# fig, ax = plt.subplots()
+#
+# candlestick_ohlc(ax, ohlc, width=0.6)
+# plt.show()
